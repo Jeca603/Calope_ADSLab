48d8b7ef7fc42186e80f451c74693d6c8024755c
Jeca603 <jecajoycalope@gmail.com>
2021-08-23 01:10:02 +0800

    Printed

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,10 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-
-Route:: get ('/addition', function(){
-    	$num1 =5;
-    	$num2 =8;
-    	$result =$num1 + $num2;
-    	return " The quotient of ".$num1." and ".$num2." is ".$result;
-    }); 
+*/ 
+Route:: get ('/subtraction', function(){
+    	$num1 =10;
+    	$num2 =1;
+    	$result =$num1 - $num2;
+    	return " The difference of ".$num1." and ".$num2." is ".$result;
+    	 }); 
